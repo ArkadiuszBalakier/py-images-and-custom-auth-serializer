ed5e5b54362c422a48b1b4488a06ee40e6663a48
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-03-05 12:09:07 +0100

    Fix Actor model first_name field by adding max_length constraint

--- cinema/models.py
+++ cinema/models.py
@@ -28,7 +28,7 @@ class Genre(models.Model):
 
 
 class Actor(models.Model):
-    first_name = models.CharField()
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
     def __str__(self):
