8176c034204ccafaecb005f49ca37e5ce7c7f75a
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-03-05 09:02:08 +0100

    Add image field to Movie model and update settings for media files
    - Added an `image` field to the `Movie` model for storing movie images.
    - Updated `settings.py` to enable timezone support and defined `MEDIA_URL` and `MEDIA_ROOT`.
    - Modified `urls.py` to serve media files during development.
    - Added a new migration for the image field in the Movie model.

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,10 @@
+import os
+
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.text import slugify
 from django.conf import settings
+from uuid import uuid4
 
 
 class CinemaHall(models.Model):
@@ -35,12 +39,18 @@ class Actor(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
+def movie_image_upload_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    return os.path.join("uploads/images/",f"{slugify(instance.title)}-{uuid4()}{extension}",)
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+    image = models.ImageField(upload_to="movie_images/", null=True, blank=True)
 
     class Meta:
         ordering = ["title"]
